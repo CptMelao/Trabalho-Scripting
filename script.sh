4f31e5d517cf262921e491ee93cd5b569387bd23
Cpt <63172596+CptMelao@users.noreply.github.com>
2023-01-20 10:27:01 +0000

    main

--- script.sh
+++ script.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
 
+# File containing URLs to scrape
+file="./urls.txt"
+
+# CSV file to save the scraped data
+csv_file="./scraped_data.csv"
+
+# Create the CSV file and add a header row
+echo "URL,Content" > $csv_file
+
+# Loop through each line in the file
+while read -r line; do
+  # Use curl to download the HTML from the URL
+  html=$(curl -s "$line")
+
+  # Use grep to search for URLs in the HTML
+  scraped_urls=$(echo "$html" | grep -o 'https*://[^"]*')
+
+  # Save the scraped data to the CSV file
+  echo "$line,$scraped_urls" >> $csv_file
+done < "$file"
