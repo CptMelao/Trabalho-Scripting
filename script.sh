2bae89c7a44dbbc2c109ebe60e85a807706d0128
Cpt <63172596+CptMelao@users.noreply.github.com>
2023-01-20 10:27:45 +0000

    alteração código

--- script.sh
+++ script.sh
@@ -7,16 +7,18 @@ file="./urls.txt"
 csv_file="./scraped_data.csv"
 
 # Create the CSV file and add a header row
-echo "URL,Content" > $csv_file
+echo "URL,Title,Description,Keywords" > $csv_file
 
 # Loop through each line in the file
 while read -r line; do
   # Use curl to download the HTML from the URL
   html=$(curl -s "$line")
 
-  # Use grep to search for URLs in the HTML
-  scraped_urls=$(echo "$html" | grep -o 'https*://[^"]*')
+  # Extract the title, description and keywords
+  title=$(echo "$html" | grep -o '<title>.*</title>' | sed 's/<title>//' | sed 's/<\/title>//')
+  description=$(echo "$html" | grep -o '<meta name="description" content=".*">' | sed 's/<meta name="description" content="//' | sed 's/">//')
+  keywords=$(echo "$html" | grep -o '<meta name="keywords" content=".*">' | sed 's/<meta name="keywords" content="//' | sed 's/">//')
 
   # Save the scraped data to the CSV file
-  echo "$line,$scraped_urls" >> $csv_file
+  echo "$line,$title,$description,$keywords" >> $csv_file
 done < "$file"
