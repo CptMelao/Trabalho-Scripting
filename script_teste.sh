36e9af78e4a6a6fcaf6363890bda717d66e81344
Cpt <63172596+CptMelao@users.noreply.github.com>
2023-01-23 12:08:44 +0000

    main

--- script_teste.sh
+++ script_teste.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
+
+# specify the URL of the webpage to scrape
+url="https://www.auto-data.net/en/porsche-carrera-gt-5.7-i-v10-40v-612hp-6692"
+
+# download the webpage
+content=$(curl -s "$url")
+
+# extract the contents of the specified tag
+specified_tag=$(echo "$content" | sed -n '/<table class="cardetailsout car2">/,/<\/div>/p')
+
+# extract the contents of every tag inside the specified tag
+tags=$(echo "$specified_tag" | sed 's/<[^>]*>//g')
 
 # print the extracted content
-echo "trs: $trs"
-echo "ths: $ths"
-echo "tds: $tds"
+echo "$tags" > output.csv
