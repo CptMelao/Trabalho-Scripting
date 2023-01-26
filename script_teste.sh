f8695437c1db32cc31826b7a86d54eeaff087260
Cpt <63172596+CptMelao@users.noreply.github.com>
2023-01-26 16:07:51 +0000

    main

--- script_teste.sh
+++ script_teste.sh
@@ -21,7 +21,7 @@ table=$(echo "$html" | sed -n '/<table[^>]*class="cardetailsout car2"[^>]*>/,/<\
 headers=$(echo "$table" | grep -o '<th>.*</th>' | sed 's/<[^>]*>//g')
 
 # Add the headers to the CSV file
-echo "$td_text" > $csv_file
+echo "$th,$td_text" > $csv_file
 
 # Extract the table rows
 rows=$(echo "$table" | sed -n '/<tr>/,/<\/tr>/p')
@@ -34,7 +34,10 @@ while read -r row; do
   # Extract the td cells
   #td=$(echo "$row" | grep -o '<td>.*</td>' | sed 's/<[^>]*>//g')
   # Extract the text inside the td cells
+  #td_text=$(echo "$row" | sed 's/<[^>]*>//g')
   td_text=$(echo "$row" | sed 's/<[^>]*>//g')
+  th=$(echo "$td_text" | grep -o '<th>.*</th>')
+  
   # Extract the text inside quotation marks inside the <td> tag
   #td_quote=$(echo "$td" | grep -o '".*"' | sed 's/"//g')
   # Extract the data from the <span class="val2"> tag
@@ -48,5 +51,5 @@ while read -r row; do
     continue
   fi
   # Write the th cell, td text,td_quote, and val2 data to the CSV file
-  echo "$td_text" >> $csv_file
+  echo "$th,$td_text" >> $csv_file
 done <<< "$rows"
